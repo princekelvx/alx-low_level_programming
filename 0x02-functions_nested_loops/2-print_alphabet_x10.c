67aeb4dea4de63be80f90b5a59df501499da233c
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-12 04:01:46 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10x
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char j;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+	}
+
+	_putchar('\n');
+	return (0);
+}
