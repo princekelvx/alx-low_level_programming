cf812953b2e5cb7810d4f53e1f47955983b59e68
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-12 12:01:22 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,7 +16,7 @@ int main(void)
 
 	for (i = 0; i < 98; i++)
 	{
-		fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 		if (i == 97)
 			printf("%ld\n", fibonacci[i]);
 		else
