1940aabcfcda84202da15f5ce9f256be900f76a5
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-12 11:48:47 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,6 +11,9 @@ int main(void)
 	int i;
 	long int fibonacci[50], sum = 2;
 
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
