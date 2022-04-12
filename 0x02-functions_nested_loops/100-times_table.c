58c89a730afeab3e4d560aa4244b935c624b2a45
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-12 06:25:37 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints time table for numbers 0 through to 14
+ *
+ * @n: input integer
+ *
+ * Return: Nil
+ */
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+				putformat(i * j);
+			_putchar('\n');
+		}
+	}
+}
+
+/**
+ * putformat - formattred characters to input
+ *
+ * @n: number being formatted
+ *
+ * Return: Nil
+ */
+
+void putformat(int n)
+{
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n < 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
