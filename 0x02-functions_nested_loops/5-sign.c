2f010b6bbc937f600bbe4441489b561a318ec6fb
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-12 05:02:12 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of numbers positive or negative
+ *
+ * @n - Instance to be checked
+ *
+ * Returns: 1 0 or -1
+ */
+
+int print_sign(int n)
+{
+	int integer = 0;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		integer = 1;
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		integer = -1;
+	}
+	else
+	{
+		_putchar('0');
+		integer = 0;
+	}
+
+	return (integer);
+}
