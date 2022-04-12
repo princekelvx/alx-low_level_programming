8314610998eac06fc46e5ede8b648e813cbc99fb
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-12 05:27:44 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,10 @@ int print_last_digit(int i)
 {
 	int n;
 
-	if (r < 0)
-		n = -1 * (r % 10);
+	if (i < 0)
+		n = -1 * (i % 10);
 	else
-		n = r % 10;
+		n = i % 10;
 
 	_putchar((n % 10) + '0');
 	return (n % 10);
