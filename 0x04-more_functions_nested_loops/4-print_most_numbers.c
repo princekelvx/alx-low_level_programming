be967a30fbb0974ba558cb28a6825fcf1ef129a2
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-14 04:35:36 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 through 9 except 2 & 4
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4)
+			_putchar(i);
+	}
+
+	_putchar('\n');
