152f875bb91fbd399a962559a36c3165ba94aa5a
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-14 04:48:43 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 through 14 10x
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i = '0';
+	int j = '0';
+
+	for (; i <= 10; i++)
+	{
+		for (; j <= 14; j++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+		}
+
+		_putchar('\n');
+	}
+}
