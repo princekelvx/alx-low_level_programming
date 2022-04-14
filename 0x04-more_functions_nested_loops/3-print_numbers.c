4391f909b28109b4589cc374b27abaf481366e86
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-14 04:15:14 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,14 @@
 /**
  * print_numbers - prints numbers 1 through 9
  *
- * Return: nothing
+ * Return: Always 0
  */
 
 void print_numbers(void)
 {
 	int i = '0';
 
-	for (; i < '10'; i++)
+	for (; i <= '9'; i++)
 		_putchar(i);
 	_putchar('\n');
-
 }
