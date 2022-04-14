cc88d0beda3ca9517389af32095665ac843e385d
princekelvx <princekelvx@gmail.com>
2022-04-14 07:10:20 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,11 +2,10 @@
 void print_integer(int m);
 
 /**
- * print_number - prints an integer
- * @n: input integer
+ * print_number - a function that prints an integer.
+ * @n: An input integer
  * Return: Nothing
  */
-
 void print_number(int n)
 {
 	if (n == 0)
