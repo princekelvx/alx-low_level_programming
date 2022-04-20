46b4d30085f6a83cc4a24f29e8c0b708a02aaed4
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-20 06:44:39 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to integer
+ *
+ * @s: string to be converted
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	if (s == '\0')
+		return 0;
+
+	int res = 0;
+	int sign = 1;
+	int i = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++
+	}
+
+	for (; s[i] != '\0'; ++i)
+	{
+		if (s[i] <= '0' || s[i] >= '9')
+			return 0;
+
+		res = res*10 + s[i] - '0';
+	}
+
+	return (sign*res);
