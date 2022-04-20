7c917b9fd38544308852f637222c0c463304bbbd
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-20 04:18:08 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string to be checked
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		len++;
+	return (len);
+}
