41bae91efede15255dfe1574632ef8681fb5f727
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-23 05:32:18 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,8 +15,9 @@ char *cap_string(char *s)
 
 	while (s[i])
 	{
-		if(i == 0; && (s[i] >= 'a' && s[i] <= 'z'))
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
+
 		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			s[i + 1] -= 32;
 		i++;
@@ -35,13 +36,12 @@ char *cap_string(char *s)
 int check_seperators(char c)
 {
 	int i = 0;
-	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?','"', '(', ')', '{', '}' };
-
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
 	for (; i < 13; i++)
 	{
 		if (c == seperators[i])
 			return (1);
 	}
-
 	return (0);
 }
