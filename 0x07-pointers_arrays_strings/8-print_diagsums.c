abd01473cf0abfa9758a77adda242d1beea0c200
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-26 04:59:00 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ void print_diagsums(int *a, int size)
 {
 	int i = 0, max = size * size, sum1 = 0, sum2 = 0;
 
-	for (; i < max; i++)
+	for (; i < max; i += size + 1)
 		sum1 += a[i];
 
 	for (i = size - 1; i < max - 1; i += size - 1)
