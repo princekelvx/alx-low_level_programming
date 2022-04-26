9ef027e00875e77aa6e45907e6537177032cadbc
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-26 03:20:39 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: memory area
+ * @src: meo=mory area
+ * @n: amount of bytes
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
