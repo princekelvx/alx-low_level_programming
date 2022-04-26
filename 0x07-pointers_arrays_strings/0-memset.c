8031d854284668994e9e58d667584d42cd2757e9
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-26 08:51:33 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset - fills the first n bytes of the memory area 
+ * _memset - fills the first n bytes of the memory area
  * pointed to by s with the constant byte b
  * @s: pointer to memory area
  * @b: constant byte
