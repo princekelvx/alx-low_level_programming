65f808f88708007e39d124d7bbda5ceb59ed164f
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-04-26 08:26:03 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,12 +8,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i = 0, j;
+	int i, j;
 
-	for (; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
+		{
+			_putchar(*(*(i + a) + j));
+		}
+	_putchar('\n');
 	}
 }
