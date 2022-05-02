367efa2337f09d6ecb6897befa6bc74585426c16
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-05-02 08:51:14 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the arguments it receives
+ * @argc: arg counter
+ * @argv: arg value
+ * Return: 0
+ */
+
+int main(int argc, **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
