4fd9dd1d8bf8eadc65273757483dafe92b44bbea
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-05-02 08:41:47 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,12 @@
  * main - function
  * @argc: offset count
  * @argv: offset value
- * Return: 0
+ * Return: 0 (Success)
  */
 
 int main(int argc, char*argv[])
 {
 	printf("%s\n", argv[argc - 1]);
+
 	return (0);
 }
