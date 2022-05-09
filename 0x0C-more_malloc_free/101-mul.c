b1e8a3e7a647b9358f086d978f6494ce96aa06eb
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-05-09 03:05:04 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two positive integers
+ * @argc: number of arguments
+ * @argv: value of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
+}
