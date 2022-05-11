4d4243460339ab65e9577d3a4bbeffd8b0cad436
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-05-11 06:35:47 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - structure for a basic dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
