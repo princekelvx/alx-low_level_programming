1c3c869ff41d19c431db469832bb349039d20eca
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-05-11 07:01:22 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner == "(nil)";
+		d->owner =  "(nil)";
 
 	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
