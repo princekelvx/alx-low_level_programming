3c167cff0052ec3488e9d56ba7cc8911b4a21bfe
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-05-13 10:10:33 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,19 +2,16 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name as is
- * @name: name of the person
- * @f: is pointer
- * Return: Nothing.
-**/
-void print_name(char *name, void (*f)(char *))
+ * print_name - prints a name
+ * @name: pointer to a name to print
+ * @f: function to print name
+ * Return: Nil
+ */
+
+void print_name(char *name, void (*f)(char *name))
 {
-if (name == NULL || f == NULL)
-{
-return;
-}
-else
-{
-f(name);
-}
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
