149bdc21ad439c8471dd7298dee5c9b075693dc6
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-05-16 04:26:23 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
 #ifndef _HEADER_MAIN_
 #define _HEADER_MAIN_
-#include <starg.h>
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
