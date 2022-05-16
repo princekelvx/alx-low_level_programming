85f12b8f72a305bbd9c37e250f5bfef0f6133028
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-05-16 01:45:40 -0700

    second commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,4 +2,6 @@
 #define _HEADER_MAIN_
 #include <starg.h>
 int sum_them_all(const unsigned int n, ...);
-#endif
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+#endif /* _HEADER_MAIN_ */
