cc74f4a625ce446c8629ce2fcdeeb7bb599c1e9b
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-05-24 08:31:05 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,13 +10,11 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	temp = head;
 	while (head)
 	{
-		temp = head;
 		head = head->next;
-		free(temp->str);
-		free(temp);
+		free(head->str);
+		free(head);
+		head = temp;
 	}
-	free (head);
 }
