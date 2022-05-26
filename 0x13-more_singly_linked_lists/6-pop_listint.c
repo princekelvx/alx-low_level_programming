3c8c49042934c49b11cc1093902b1e0a2d1a422e
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-05-26 08:19:25 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,14 +9,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
-	int data;
+	int n;
 
-	if (*head = NULL)
+	if (*head == NULL)
 		return (0);
 
-	temp = *head;
-	*head = tp->next;
-	data = temp->n;
-	free(temp);
-	return (data);
+	n = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	return (n);
 }
