febff6841477471eaa98c512a5210f030089e952
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-05-26 05:04:09 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
-	new_node = head;
+	new_node = *head;
 	*head = malloc(sizeof(listint_t));
 	if (!*head)
 		return (NULL);
