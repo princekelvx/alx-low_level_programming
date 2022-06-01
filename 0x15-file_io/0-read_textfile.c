b70221cccddb294ecb8bdcad59aafc8b5cf5a1b9
Kelvin <princekelvs@gmail.com>
2022-06-01 05:16:14 -0700

    FILE I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,37 @@
 #include "main.h"
-
 /**
- * read_textfile - reads the contents of a .txt file and prints it to stdout
- * @filename: name of the file
- * @letters: number of letters expected to read (for memory allocation)
- * Return: the number of letters printed, 0 on failure
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, rcont, wcont;
-	char *cont;
-
-	if (filename == NULL)
+	int fd;
+	int i, y;
+	char *buf;
+	if (!filename)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-	cont = malloc(sizeof(char) * letters);
-
-	if (cont == NULL)
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
 		return (0);
-
-	rcont = read(fd, cont, letters);
-	if (rcont < 0)
+	i = read(fd, buf, letters);
+	if (i < 0)
 	{
-		free(cont);
+		free(buf);
 		return (0);
 	}
-	cont[rcont] = '\0';
+	buf[i] = '\0';
 	close(fd);
-	wcont = write(STDOUT_FILENO, cont, rcont);
-	if (wcont < 0)
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
 	{
-		free(cont);
+		free(buf);
 		return (0);
 	}
-	free(cont);
-	return (wcont);
+	free(buf);
+	return (y);
 }
-
