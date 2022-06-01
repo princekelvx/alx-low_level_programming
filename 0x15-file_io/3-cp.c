939cf89d9c72cc295e18ab880439bc7d5b2019ea
Kelvin <princekelvs@gmail.com>
2022-06-01 05:02:36 -0700

    FILE I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,9 +7,10 @@
  * Return: Always 0 Success
  */
 
+
 int main(int argc, char *argv[])
 {
-	int fd_read, fd_write, rcont, m, n;
+	int fd_r, fd_w, x, m, n;
 	char buf[BUFSIZ];
 
 	if (argc != 3)
@@ -17,35 +18,35 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_read = open(argv[1], O_RDONLY);
-	if (fd_read < 0)
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from this file%s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_write = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((rcont = read(fd_read, buf, BUFSIZ)) > 0)
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((x = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-		if (fd_write < 0 || write(fd_write, buf, rcont) != rcont)
+		if (fd_w < 0 || write(fd_w, buf, x) != x)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to this file %s\n", argv[2]);
-			close(fd_read);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_r);
 			exit(99);
 		}
 	}
-	if (rcont < 0)
+	if (x < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from this file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	m = close(fd_read);
-	n = close(fd_write);
+	m = close(fd_r);
+	n = close(fd_w);
 	if (m < 0 || n < 0)
 	{
 		if (m < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
 		if (n < 0)
-			dprintf(STDERR_FILENO, "Error: Can't write fd %d\n", fd_write);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
 	return (0);
